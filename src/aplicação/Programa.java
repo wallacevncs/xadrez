fa4d35f41ad3e30550a74a3510464e3356b5f151
Wallace Vinicius <wallacevncs@gmail.com>
2020-01-02 16:25:57 -0300

    Primeira classe: Posicao

--- src/aplicação/Programa.java
+++ src/aplicação/Programa.java
@@ -1,10 +1,14 @@
 package aplicação;
 
+import JogoDeTabuleiro.Posição;
+
 public class Programa {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Hello");
+		Posição pos = new Posição(3,5);
+		
+		System.out.println(pos);
 	}
 
 }
